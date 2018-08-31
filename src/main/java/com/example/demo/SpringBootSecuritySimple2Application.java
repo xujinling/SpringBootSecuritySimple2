37783ac8df5e689e559b9e465da0750a5c02bc89
xujinling <1870046354@qq.com>
2018-08-31 14:16:12 +0800

    SpringBootSecuritySimple2

---
+++ src/main/java/com/example/demo/SpringBootSecuritySimple2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecuritySimple2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecuritySimple2Application.class, args);
+	}
+}
